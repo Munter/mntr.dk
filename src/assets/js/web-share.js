4b65bdc6b23e17889c2981bb761f66abf830a77c
Peter Müller <munter@fumle.dk>
2016-10-26 22:44:19 +0200

    Add title to web share and unescape correctly

--- src/assets/js/web-share.js
+++ src/assets/js/web-share.js
@@ -5,11 +5,16 @@ if (typeof navigator.share === 'function') {
         anchor.textContent = 'Share this';
 
         anchor.addEventListener('click', function (e) {
-            var text = decodeURIComponent(anchor.href.split('?').pop().split('&').shift().split('=').pop());
+            var queryParams = anchor.href.split('?').pop().split('&').reduce(function (prev, string) {
+                var pair = string.split('=');
+
+                prev[pair[1]] = decodeURIComponent(pair[2]).replace('+', ''); // Jekyll cgi_escape adds '+' instead or '%20'
+            }, {});
+
             navigator.share({
-                title: document.title,
-                text: text,
-                url: document.querySelector('link[rel=canonical]') ? document.querySelector('link[rel=canonical]').href : window.location.href
+                title: queryParams.title,
+                text: queryParams.text,
+                url: queryParams.url
             })
 
             e.preventDefault();
