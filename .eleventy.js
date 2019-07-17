41e1efe828183418cc14630220e8a45468dab9bd
Peter Müller <munter@fumle.dk>
2019-07-17 10:52:36 +0200

    Move files around to get the right directory structure

--- .eleventy.js
+++ .eleventy.js
@@ -1,21 +1,14 @@
-const globby = require('globby');
-const { relative, basename } = require('path');
-
 module.exports = function(eleventyConfig) {
   eleventyConfig.addLayoutAlias('default', 'layouts/default.html');
   eleventyConfig.addLayoutAlias('post', 'layouts/post.html');
   eleventyConfig.addLayoutAlias('talk', 'layouts/talk.html');
 
-  eleventyConfig.addCollection("allMyContent", function(collection) {
-    return collection.getAll();
-  });
-
   eleventyConfig.addCollection("posts", function(collection) {
-    return collection.getFilteredByGlob("src/_posts/*.md").reverse();
+    return collection.getFilteredByGlob("src/posts/*.md").reverse();
   });
 
   eleventyConfig.addCollection("talks", function(collection) {
-    return collection.getFilteredByGlob("src/_talks/*.md").reverse();
+    return collection.getFilteredByGlob("src/talks/*.md").reverse();
   });
 
   return {
